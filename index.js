6c1a84ea34b0be9634c277ebd7e85a24ff132e5f
Andrew Masih <armasih@outlook.com>
2021-06-29 22:14:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/* the most common type of 'Loops' are the "FOR LOOP" and "WHILE LOOP".
+
+Iteration:
+
+Iterations are performed in LOOPS,
+
+Iteration do the same step over and over again, untill a certain condition is met.
+
+We can tell the computer to take 5 steps in 1 direction until it gets home:
+
+step.forward (5) until home;
+
+This means step.forward(5) is going to repeat over and over again until it gets home. 
+
+for ([initialExpression]; [conditionExpression]; [incrementExpression]) statement
+
+*/
+
+
+for (let step = 0; step < 5; step++) {
+  // Runs 5 times, with values of step 0 through 4.
+  console.log('Walking forward one step to get home');
+}
+
